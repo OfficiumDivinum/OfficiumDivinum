80073798b7fe00f602369063018be3fa01afed20
John Maximilian <2e0byo@gmail.com>
2021-02-21 17:56:04 +0000

    ordinals and old date template methods

--- backend/app/app/crud/crud_martyrology.py
+++ backend/app/app/crud/crud_martyrology.py
@@ -1,24 +1,24 @@
-import json
 from typing import Optional
 
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
+from app import schemas
 from app.crud.base import CRUDBase
-from app.models.martyrology import Martyrology
+from app.models.martyrology import Martyrology, OldDateTemplate, Ordinals
 from app.schemas.martyrology import MartyrologyCreate, MartyrologyUpdate
 
 
 class CRUDMartyrology(CRUDBase[Martyrology, MartyrologyCreate, MartyrologyUpdate]):
-    def create(self, db: Session, *, obj_in: MartyrologyCreate) -> Martyrology:
-        obj_in_data = jsonable_encoder(obj_in)
-        print(json.dumps(obj_in_data, indent=2))
-        db_obj = self.model(**obj_in_data)
-        print(jsonable_encoder(db_obj))
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
-        return db_obj
+    # def create(self, db: Session, *, obj_in: MartyrologyCreate) -> Martyrology:
+    #     obj_in_data = jsonable_encoder(obj_in)
+    #     print(json.dumps(obj_in_data, indent=2))
+    #     db_obj = self.model(**obj_in_data)
+    #     print(jsonable_encoder(db_obj))
+    #     db.add(db_obj)
+    #     db.commit()
+    #     db.refresh(db_obj)
+    #     return db_obj
 
     def get_by_datestr(self, db: Session, *, datestr: str) -> Optional[Martyrology]:
         return db.query(Martyrology).filter(Martyrology.datestr == datestr)
@@ -30,3 +30,25 @@ class CRUDMartyrology(CRUDBase[Martyrology, MartyrologyCreate, MartyrologyUpdate
 
 
 martyrology = CRUDMartyrology(Martyrology)
+
+
+class CRUDOrdinals(
+    CRUDBase[Ordinals, schemas.martyrology.Ordinals, schemas.martyrology.Ordinals]
+):
+    pass
+
+
+ordinals = CRUDOrdinals(Ordinals)
+
+
+class CRUDOldDateTemplate(
+    CRUDBase[
+        OldDateTemplate,
+        schemas.martyrology.OldDateTemplate,
+        schemas.martyrology.OldDateTemplate,
+    ]
+):
+    pass
+
+
+old_date_template = CRUDOldDateTemplate(OldDateTemplate)
--- backend/app/app/schemas/martyrology.py
+++ backend/app/app/schemas/martyrology.py
@@ -8,17 +8,7 @@ from .office_parts import BlockBase, LineBase
 class MartyrologyBase(BlockBase):
     datestr: str
     title: str
-
-
-class Ordinals(BaseModel):
-    content: List
     language: str
-
-
-class OldDateTemplate(BaseModel):
-    content: str
-    language: str
-    ordinals: Ordinals
 
 
 class MartyrologyCreate(MartyrologyBase):
@@ -34,7 +24,7 @@ class MartyrologyInDBBase(MartyrologyBase):
     id: int
     title: str
     old_date_template_id: Optional[int]
-    parts: str
+    parts: List[LineBase]
 
     class Config:
         orm_mode = True
@@ -48,3 +38,20 @@ class Martyrology(MartyrologyInDBBase):
 # Properties properties stored in DB
 class MartyrologyInDB(MartyrologyInDBBase):
     pass
+
+
+class Ordinals(BaseModel):
+    content: List
+    language: str
+
+    class Config:
+        orm_mode = True
+
+
+class OldDateTemplate(BaseModel):
+    content: str
+    language: str
+    ordinals: Ordinals
+
+    class Config:
+        orm_mode = True
