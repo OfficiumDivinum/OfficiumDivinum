f49f10323b60dfc21ad106d17541ee811ed56c07
John Maximilian <2e0byo@gmail.com>
2021-02-21 20:58:48 +0000

    factory function and use

---
+++ backend/app/app/api/api_v1/endpoints/item_base.py
@@ -0,0 +1,101 @@
+from typing import Any, List, Optional
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi_utils.cbv import cbv
+from sqlalchemy.orm import Session
+
+from app import models
+from app.api import deps
+from app.crud.base import CreateSchemaType, CRUDType, SchemaType, UpdateSchemaType
+
+
+def create_item_crud(
+    item_schema: SchemaType,
+    item_crud: CRUDType,
+    item_update_schema: Optional[UpdateSchemaType] = None,
+    item_create_schema: Optional[CreateSchemaType] = None,
+):
+    if not item_update_schema:
+        item_update_schema = item_schema
+    if not item_create_schema:
+        item_create_schema = item_update_schema
+
+    router = APIRouter()
+
+    @cbv(router)
+    class ItemCBV:
+        db: Session = Depends(deps.get_db)
+        current_user: models.User = Depends(deps.get_current_active_user)
+
+        @router.get("/", response_model=List[item_schema])
+        def read_items(
+            self,
+            skip: int = 0,
+            limit: int = 100,
+        ) -> Any:
+            """Retrieve items."""
+            if self.current_user.is_superuser(self.current_user):
+                items = item_crud.get_multi(self.db, skip=skip, limit=limit)
+            else:
+                items = item_crud.get_multi_by_owner(
+                    db=self.db, owner_id=self.current_user.id, skip=skip, limit=limit
+                )
+            return items
+
+        @router.post("/", response_model=item_schema)
+        def create_item(self, *, item_in: item_create_schema) -> Any:
+            """Create new item."""
+            item = item_crud.create_with_owner(
+                db=self.db, obj_in=item_in, owner_id=self.current_user.id
+            )
+            return item
+
+        @router.put("/{id}", response_model=item_schema)
+        def update_item(
+            self,
+            id: int,
+            item_in: item_update_schema,
+        ) -> Any:
+            """Update an item."""
+            item = item_crud.get(db=self.db, id=id)
+            if not item:
+                raise HTTPException(status_code=404, detail="Item not found")
+            if not self.current_user.is_superuser(self.current_user) and (
+                item.owner_id != self.current_user.id
+            ):
+                raise HTTPException(status_code=400, detail="Not enough permissions")
+            item = item_crud.update(db=self.db, db_obj=item, obj_in=item_in)
+            return item
+
+        @router.get("/{id}", response_model=item_schema)
+        def read_item(
+            self,
+            id: int,
+        ) -> Any:
+            """Get item by ID."""
+            item = item_crud.get(db=self.db, id=id)
+            if not item:
+                raise HTTPException(status_code=404, detail="Item not found")
+            if not self.current_user.is_superuser(self.current_user) and (
+                item.owner_id != self.current_user.id
+            ):
+                raise HTTPException(status_code=400, detail="Not enough permissions")
+            return item
+
+        @router.delete("/{id}", response_model=item_schema)
+        def delete_item(
+            self,
+            id: int,
+        ) -> Any:
+            """Delete an item."""
+            item = item_crud.get(db=self.db, id=id)
+            if not item:
+                raise HTTPException(status_code=404, detail="Item not found")
+            if not self.current_user.is_superuser(self.current_user) and (
+                item.owner_id != self.current_user.id
+            ):
+                raise HTTPException(status_code=400, detail="Not enough permissions")
+            item = item_crud.remove(db=self.db, id=id)
+            return item
+
+    return router
--- backend/app/app/api/api_v1/endpoints/martyrology.py
+++ backend/app/app/api/api_v1/endpoints/martyrology.py
@@ -1,97 +1,20 @@
-from typing import Any, List
+from app import crud, schemas
 
-from fastapi import APIRouter, Depends, HTTPException
-from fastapi_utils.cbv import cbv
-from sqlalchemy.orm import Session
-
-from app import crud, models, schemas
-from app.api import deps
-
-router = APIRouter()
+from .item_base import create_item_crud
 
 item_schema = schemas.Martyrology
 item_update_schema = schemas.MartyrologyUpdate
 item_create_schema = schemas.MartyrologyCreate
 item_crud = crud.martyrology
 
+martyrology_router = create_item_crud(
+    item_schema, item_crud, item_update_schema, item_create_schema
+)
 
-@cbv(router)
-class ItemCBV:
-    db: Session = Depends(deps.get_db)
-    current_user: models.User = Depends(deps.get_current_active_user)
+ordinals_router = create_item_crud(schemas.Ordinals, crud.ordinals)
 
-    @router.get("/", response_model=List[schemas.Item])
-    def read_items(
-        self,
-        skip: int = 0,
-        limit: int = 100,
-    ) -> Any:
-        """Retrieve items."""
-        if self.current_user.is_superuser(self.current_user):
-            items = item_crud.get_multi(self.db, skip=skip, limit=limit)
-        else:
-            items = item_crud.get_multi_by_owner(
-                db=self.db, owner_id=self.current_user.id, skip=skip, limit=limit
-            )
-        return items
-
-    @router.post("/", response_model=schemas.Item)
-    def create_item(self, *, item_in: item_schema) -> Any:
-        """Create new item."""
-        item = item_crud.create_with_owner(
-            db=self.db, obj_in=item_in, owner_id=self.current_user.id
-        )
-        return item
-
-    @router.put("/{id}", response_model=schemas.Item)
-    def update_item(
-        self,
-        id: int,
-        item_in: item_schema,
-    ) -> Any:
-        """Update an item."""
-        item = item_crud.get(db=self.db, id=id)
-        if not item:
-            raise HTTPException(status_code=404, detail="Item not found")
-        if not self.current_user.is_superuser(self.current_user) and (
-            item.owner_id != self.current_user.id
-        ):
-            raise HTTPException(status_code=400, detail="Not enough permissions")
-        item = item_crud.update(db=self.db, db_obj=item, obj_in=item_in)
-        return item
-
-    @router.get("/{id}", response_model=schemas.Item)
-    def read_item(
-        self,
-        id: int,
-    ) -> Any:
-        """Get item by ID."""
-        item = item_crud.get(db=self.db, id=id)
-        if not item:
-            raise HTTPException(status_code=404, detail="Item not found")
-        if not self.current_user.is_superuser(self.current_user) and (
-            item.owner_id != self.current_user.id
-        ):
-            raise HTTPException(status_code=400, detail="Not enough permissions")
-        return item
-
-    @router.delete("/{id}", response_model=schemas.Item)
-    def delete_item(
-        self,
-        id: int,
-    ) -> Any:
-        """Delete an item."""
-        item = item_crud.get(db=self.db, id=id)
-        if not item:
-            raise HTTPException(status_code=404, detail="Item not found")
-        if not self.current_user.is_superuser(self.current_user) and (
-            item.owner_id != self.current_user.id
-        ):
-            raise HTTPException(status_code=400, detail="Not enough permissions")
-        item = item_crud.remove(db=self.db, id=id)
-        return item
-
-    # return
-
-
-# cbv(router)(ItemCBV(schemas.Martyrology, crud.martyrology, schemas.MartyrologyCreate))
+martyrology_router.include_router(
+    ordinals_router,
+    prefix="/ordinals",
+    tags=["martyrology-ordinals"],
+)
