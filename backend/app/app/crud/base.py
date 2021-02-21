b1d6464d0eeb71db3e29d03a97832d8009790612
John Maximilian <2e0byo@gmail.com>
2021-02-21 21:18:01 +0000

    crud with owner base class

--- backend/app/app/crud/base.py
+++ backend/app/app/crud/base.py
@@ -68,3 +68,26 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
 
 
 CRUDType = TypeVar("CRUDType", bound=CRUDBase)
+
+
+class CRUDWithOwnerBase(CRUDBase[ModelType, CreateSchemaType, UpdateSchemaType]):
+    def create_with_owner(
+        self, db: Session, *, obj_in: CreateSchemaType, owner_id: int
+    ) -> ModelType:
+        obj_in_data = jsonable_encoder(obj_in)
+        db_obj = self.model(**obj_in_data, owner_id=owner_id)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def get_multi_by_owner(
+        self, db: Session, *, owner_id: int, skip: int = 0, limit: int = 100
+    ) -> List[ModelType]:
+        return (
+            db.query(self.model)
+            .filter(self.model.owner_id == owner_id)
+            .offset(skip)
+            .limit(limit)
+            .all()
+        )
--- backend/app/app/crud/crud_martyrology.py
+++ backend/app/app/crud/crud_martyrology.py
@@ -4,22 +4,14 @@ from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
 from app import schemas
-from app.crud.base import CRUDBase
+from app.crud.base import CRUDBase, CRUDWithOwnerBase
 from app.models.martyrology import Martyrology, OldDateTemplate, Ordinals
 from app.schemas.martyrology import MartyrologyCreate, MartyrologyUpdate
 
 
-class CRUDMartyrology(CRUDBase[Martyrology, MartyrologyCreate, MartyrologyUpdate]):
-    # def create(self, db: Session, *, obj_in: MartyrologyCreate) -> Martyrology:
-    #     obj_in_data = jsonable_encoder(obj_in)
-    #     print(json.dumps(obj_in_data, indent=2))
-    #     db_obj = self.model(**obj_in_data)
-    #     print(jsonable_encoder(db_obj))
-    #     db.add(db_obj)
-    #     db.commit()
-    #     db.refresh(db_obj)
-    #     return db_obj
-
+class CRUDMartyrology(
+    CRUDWithOwnerBase[Martyrology, MartyrologyCreate, MartyrologyUpdate]
+):
     def get_by_datestr(self, db: Session, *, datestr: str) -> Optional[Martyrology]:
         return db.query(Martyrology).filter(Martyrology.datestr == datestr)
 
@@ -33,7 +25,9 @@ martyrology = CRUDMartyrology(Martyrology)
 
 
 class CRUDOrdinals(
-    CRUDBase[Ordinals, schemas.martyrology.Ordinals, schemas.martyrology.Ordinals]
+    CRUDWithOwnerBase[
+        Ordinals, schemas.martyrology.Ordinals, schemas.martyrology.Ordinals
+    ]
 ):
     pass
 
@@ -42,7 +36,7 @@ ordinals = CRUDOrdinals(Ordinals)
 
 
 class CRUDOldDateTemplate(
-    CRUDBase[
+    CRUDWithOwnerBase[
         OldDateTemplate,
         schemas.martyrology.OldDateTemplate,
         schemas.martyrology.OldDateTemplate,
