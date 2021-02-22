aa4ab1e9cea79dc692dab0f2713fea081c17fb10
John Maximilian <2e0byo@gmail.com>
2021-02-22 02:17:27 +0000

    alembic

---
+++ backend/app/alembic/versions/ae4bb7688a73_martyrology_crud_ready.py
@@ -0,0 +1,24 @@
+"""Martyrology CRUD ready
+
+Revision ID: ae4bb7688a73
+Revises: d4867f3a4c0a
+Create Date: 2021-02-21 22:43:39.450317
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ae4bb7688a73'
+down_revision = 'd4867f3a4c0a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ backend/app/alembic/versions/cb17e78c4ee8_add_julian_date_column.py
@@ -0,0 +1,24 @@
+"""add julian_date column
+
+Revision ID: cb17e78c4ee8
+Revises: ae4bb7688a73
+Create Date: 2021-02-21 23:02:20.805610
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cb17e78c4ee8'
+down_revision = 'ae4bb7688a73'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
