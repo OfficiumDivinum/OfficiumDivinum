22edbad8688bb5ae44ecffa9ad43e0c4a8b3b151
John Maximilian <2e0byo@gmail.com>
2021-02-23 15:03:15 +0000

    worker tasks added

--- backend/app/app/core/celery_app.py
+++ backend/app/app/core/celery_app.py
@@ -1,5 +1,12 @@
 from celery import Celery
 
-celery_app = Celery("worker", broker="amqp://guest@queue//")
+celery_app = Celery(
+    "worker", broker="amqp://guest@queue//", backend="amqp://guest@queue//"
+)
 
-celery_app.conf.task_routes = {"app.worker.test_celery": "main-queue"}
+celery_app.conf.task_routes = {
+    "app.worker.test_celery": "main-queue",
+    "app.worker.resolve_datestrs": "main-queue",
+    "app.worker.resolve_datestr": "main-queue",
+    "app.worker.linear_resolve_datestrs": "main-queue",
+}
