e79b616f70b2b3493749dabdffe5d22e17ab8c7b
John Maximilian <2e0byo@gmail.com>
2021-02-24 14:42:02 +0000

    be sure to import for alembic

--- backend/app/app/db/base.py
+++ backend/app/app/db/base.py
@@ -2,4 +2,5 @@
 # imported by Alembic
 from app.db.base_class import Base  # noqa
 from app.models.item import Item  # noqa
+from app.models.martyrology import *  # noqa
 from app.models.user import User  # noqa
