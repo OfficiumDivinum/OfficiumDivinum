9c71204d08cd86a7277830d086e47340ab95f5ae
John Maximilian <2e0byo@gmail.com>
2021-02-26 17:05:32 +0000

    maybe t2obj works now...

--- backend/app/app/parsers/T2obj.py
+++ backend/app/app/parsers/T2obj.py
@@ -1,11 +1,11 @@
 """Parse Divinumofficium's Temporal files into Feast objects."""
+import re
+from pathlib import Path
 
-from ..DSL import days, months, ordinals, specials
+from devtools import debug
 
-# from ..objects import Date
-# from ..objects import Feast
-# from ..objects import Octave
-# from ..objects import Rank
+from app.DSL import days, months, ordinals, specials
+from app.schemas.calendar import FeastCreate
 
 
 def parse_DO_sections(lines: list) -> list:
@@ -46,96 +46,94 @@ def parse_DO_sections(lines: list) -> list:
     return sections
 
 
-# def parse_file(fn: Path, calendar: str) -> Feast:
-#     """
-#     Parse provided file.
+def parse_file(fn: Path, calendar: str) -> FeastCreate:
+    """
+    Parse provided file.
 
-#     Parameters
-#     ----------
-#     fn: Path : File to parse.
+    Parameters
+    ----------
+    fn: Path : File to parse.
 
-#     calendar : str: Calendar to use (mainly for naming at this stage).
+    calendar : str: Calendar to use (mainly for naming at this stage).
 
 
-#     Returns
-#     -------
-#     A Feast object represeting the day.
-#     """
-#     try:
-#         after, day = fn.stem.split("-")
-#     except ValueError:
-#         return  # give up
-#     qualifiers = None
-#     name = None
-#     try:
-#         int(after)
-#         date = after[:-1]
-#         week = int(after[-1])
-#     except ValueError:
-#         date, week = re.findall(r"([A-Z][a-z]+)([0-9]+)", after)[0]
-#     try:
-#         day = int(day)
-#     except ValueError:
-#         day, qualifiers = re.findall(r"([0-9])(.+)", day)[0]
+    Returns
+    -------
+    A Feast object represeting the day.
+    """
+    try:
+        after, day = fn.stem.split("-")
+    except ValueError:
+        return  # give up
+    qualifiers = None
+    name = None
+    try:
+        int(after)
+        date = after[:-1]
+        week = int(after[-1])
+    except ValueError:
+        date, week = re.findall(r"([A-Z][a-z]+)([0-9]+)", after)[0]
+    try:
+        day = int(day)
+    except ValueError:
+        day, qualifiers = re.findall(r"([0-9])(.+)", day)[0]
 
-#     try:
-#         date = f"1 {months[int(date) - 1]}"
-#     except ValueError:
-#         # for non month temporal it the reference *might* be a Sunday (e.g. Easter).
-#         date = specials[date]
-#     datestr = f"{ordinals[int(week)]} Sun after {date}"
-#     # datestr = f"{ordinals[week]} Sun on or after {date}"
-#     day = int(day)
-#     if day != 0:
-#         datestr = f"{days[day]} after {datestr}"
+    try:
+        date = f"1 {months[int(date) - 1]}"
+    except ValueError:
+        # for non month temporal it the reference *might* be a Sunday (e.g. Easter).
+        date = specials[date]
+    datestr = f"{ordinals[int(week)]} Sun after {date}"
+    # datestr = f"{ordinals[week]} Sun on or after {date}"
+    day = int(day)
+    if day != 0:
+        datestr = f"{days[day]} after {datestr}"
 
-#     lines = fn.open().readlines()
-#     sections = parse_DO_sections(lines)
-#     rank = "Feria"
-#     try:
-#         name, rank, rankno, source = [
-#             *sections["Rank1960"][0].split(";;"),
-#             None,
-#             None,
-#             None,
-#             None,
-#         ][:4]
+    lines = fn.open().readlines()
+    sections = parse_DO_sections(lines)
+    rank = "Feria"
+    try:
+        name, rank, rankno, source = [
+            *sections["Rank1960"][0].split(";;"),
+            None,
+            None,
+            None,
+            None,
+        ][:4]
 
-#     except KeyError:
-#         try:
-#             name, rank, rankno, source = [
-#                 *sections["Rank"][0].split(";;"),
-#                 None,
-#                 None,
-#                 None,
-#                 None,
-#             ][:4]
-#         except KeyError:
-#             pass
-#     try:
-#         rank, octave = rank.split("cum")
-#         privileged = True if "privilegiata" in octave else False
-#         octave_rank = re.findall(r"Octava ([Pp]rivilegiata)* (.*)", octave)[0][1]
-#         rank = Rank(
-#             rank, octave=Octave(octave, privileged=privileged, rank=Rank(octave_rank))
-#         )
-#     except ValueError:
-#         rank = Rank(rank)
-#     celebration = None
-#     commemorations = None
+    except KeyError:
+        try:
+            name, rank, rankno, source = [
+                *sections["Rank"][0].split(";;"),
+                None,
+                None,
+                None,
+                None,
+            ][:4]
+        except KeyError:
+            pass
+    try:
+        rank, octave = rank.split("cum")
+        # privileged = True if "privilegiata" in octave else False
+        # octave_ = re.findall(r"Octava ([Pp]rivilegiata)* (.*)", octave)[0][1]
 
-#     for section, content in sections.items():
-#         if section == "Rule":
-#             pass
-#         # pass  # later implement handling here
+        # rank = Rank(
+        #     rank, octave=Octave(octave, privileged=privileged, rank=Rank(octave_rank))
+        # )
+    except ValueError:
+        pass
+    commemorations = None
 
-#     return Feast(
-#         rank,
-#         Date(datestr),
-#         calendar,
-#         "de Tempore",
-#         name,
-#         celebration,
-#         commemorations,
-#         qualifiers,
-#     )
+    for section, content in sections.items():
+        if section == "Rule":
+            debug(content)
+        # pass  # later implement handling here
+
+    return FeastCreate(
+        name=name,
+        type_="de Tempore",
+        datestr=datestr,
+        rank_name=rank,
+        calendar=calendar,
+        commemorations=commemorations,
+    )
