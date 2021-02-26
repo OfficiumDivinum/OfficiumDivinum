93f1e9502ce7f8a15eeed772179959bbccec0ef7
John Maximilian <2e0byo@gmail.com>
2021-02-26 12:53:28 +0000

    calendar schemas

---
+++ backend/app/app/schemas/calendar.py
@@ -0,0 +1,58 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class CommemorationBase(BaseModel):
+    """Common Commemoration properties."""
+
+    name: str
+    version: str
+    rank_name: str
+    rank_defeatable: bool
+
+
+class CommemorationCreate(CommemorationBase):
+    """Properties to recieve on creation."""
+
+
+class CommemorationInDBBase(CommemorationBase):
+    """Commemoration in DB properties."""
+
+    id: int
+    owner_id: int
+
+    class config:
+        orm_mode = True
+
+
+class Commemoration(CommemorationInDBBase):
+    """Properties to return to client."""
+
+
+class FeastBase(BaseModel):
+    """Common feast properties."""
+
+    name: str
+    type_: str
+    version: str
+    rank_name: str
+    rank_defeatable: str
+    commemorations: Optional[List[Commemoration]]
+    octave: str
+
+
+class FeastCreate(FeastBase):
+    """Properties to recieve on creation."""
+
+
+class FeastInDBBase(FeastBase):
+    """Feast in DB properties."""
+
+    id: int
+    owner_id: int
+
+    class config:
+        orm_mode = True
+
+class Feast(FeastInDBBase)
