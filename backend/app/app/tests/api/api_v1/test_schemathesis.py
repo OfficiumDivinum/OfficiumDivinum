cc36f758efecf011c1ea39c0739e70e22e681c51
John Maximilian <2e0byo@gmail.com>
2021-02-26 22:40:36 +0000

    a shot at munging the testsuite

--- backend/app/app/tests/api/api_v1/test_schemathesis.py
+++ backend/app/app/tests/api/api_v1/test_schemathesis.py
@@ -4,7 +4,25 @@ from hypothesis import settings
 from app.main import app
 
 schemathesis.fixups.install()
+
+
+@schemathesis.hooks.register
+def before_load_schema(context, raw_schema):
+    """Remove the /users endpoint entirely, as we test that elsewhere, and trashing the
+    first user doesn't help."""
+    print(raw_schema["paths"].keys())
+
+    del raw_schema["paths"]["/api/v1/users/"]
+
+
 schema = schemathesis.from_asgi("/api/v1/openapi.json", app)
+
+schema.add_link(
+    source=schema["/api/v1/items/"]["POST"],
+    target=schema["/api/v1/items/{id}"]["GET"],
+    status_code="201",
+    parameters={"id": "$response.body#/id"},
+)
 
 
 @schema.parametrize()
