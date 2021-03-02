616dc687150b51c44d1f1c622143cc883555f411
John Maximilian <2e0byo@gmail.com>
2021-03-02 19:35:10 +0000

    link follower for DO

---
+++ backend/app/app/parsers/deref.py
@@ -0,0 +1,27 @@
+"""Dereferencer for DO links."""
+import re
+from pathlib import Path
+
+from devtools import debug
+
+
+def deref(linkstr: str, originf: Path):
+    assert linkstr.startswith("@")
+    linkstr, part = re.search(r"@(.*?):(.*):*", linkstr).groups()
+    if "s/" in part:
+        part = re.search(r"(.*):s/.*", part).groups()[0]
+    if not linkstr:
+        targetf = originf
+    if "/" in linkstr:
+        targetf = originf.parent.parent / f"{linkstr}.txt"
+    else:
+        targetf = originf
+
+    return targetf, part
+
+
+# def resolve_anything(targetf, part, lang):
+#     if "Hymnus" in part:
+#         from .H2obj import parse_file
+#         hymns = parse_file(targetf, lang)
+#         return [hymn for hymn in hymns if hymn.title==]
