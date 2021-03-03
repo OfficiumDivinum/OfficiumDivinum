aa16fbe5dd1e9cd112638b193fdf0058c8a8e379
John Maximilian <2e0byo@gmail.com>
2021-03-03 11:20:49 +0000

    add testsuite for parsers

--- backend/app/app/tests/parsers/test_parsers.py
+++ backend/app/app/tests/parsers/test_parsers.py
@@ -1,2 +1,14 @@
+from pathlib import Path
+
+from app import run_parsers
+
+
 def test_parsers():
     """Test all the parsers."""
+    root = Path("./test-DO-data")
+    lang = "Latin"
+    versions = ["1960"]
+
+    # test without upload
+    for version in versions:
+        run_parsers.parse_upload(root, lang, version, pokemon=True, test=True)
