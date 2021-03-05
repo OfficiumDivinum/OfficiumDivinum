c664917ff6a50202579d271882a7d4dedf4766e4
John Maximilian <2e0byo@gmail.com>
2021-03-05 12:56:15 +0000

    fuzz test parsers

---
+++ backend/app/app/tests/parsers/fuzz_test_parsers.py
@@ -0,0 +1,108 @@
+# This test code was written by the `hypothesis.extra.ghostwriter` module
+# and is provided under the Creative Commons Zero public domain dedication.
+import string
+from pathlib import Path
+from random import randint
+
+from hypothesis import given
+from hypothesis import strategies as st
+
+import app.parsers.util
+
+
+def make_linkstr(target, part, replacement, squash=True):
+    if replacement:
+        replacement = f"s/{replacement}//"
+    if squash and replacement:
+        replacement += "s"
+    linkstr = f"@{target}:{part}:{replacement}"
+    return linkstr
+
+
+def make_both(target, part, replacement, squash=True):
+    content = replacement
+    if replacement:
+        replacement = f"s/{replacement}//"
+    if squash and replacement:
+        replacement += "s"
+    linkstr = f"@{target}:{part}:{replacement}"
+    return linkstr, [[content]]
+
+
+def sane_text(**kwargs):
+    kwargs["alphabet"] = string.ascii_letters
+    return st.text(**kwargs)
+
+
+@st.composite
+def make_linkstr_content(
+    draw,
+):
+    linkstr, content = draw(
+        st.builds(make_both, sane_text(), sane_text(), sane_text(min_size=4))
+    )
+    return (linkstr, content)
+
+
+@given(
+    linkstr=st.builds(make_linkstr, sane_text(), sane_text(), sane_text()),
+    originf=st.builds(Path),
+)
+def test_fuzz_deref(linkstr, originf):
+    app.parsers.util.deref(linkstr=linkstr, originf=originf)
+
+
+@given(fn=st.builds(Path))
+def test_fuzz_guess_section_header(fn):
+    app.parsers.util.guess_section_header(fn=fn)
+
+
+@given(lines=st.builds(list), section_header_regex=st.just("\\[(.*)\\]"))
+def test_fuzz_parse_DO_sections(lines, section_header_regex):
+    app.parsers.util.parse_DO_sections(
+        lines=lines, section_header_regex=section_header_regex
+    )
+
+
+def version(v):
+    versions = ["1960"]
+    return versions[v]
+
+
+def getf():
+    files = list(Path("app/tests/parsers/test-DO-data/").glob("**/*.txt"))
+    print(files)
+    return files[randint(0, len(files))]
+
+
+@given(
+    fn=st.builds(getf),
+    section_key=sane_text(),
+    version=st.builds(version, st.integers(0, 0)),
+    follow_links=st.booleans(),
+    follow_only_interesting_links=st.booleans(),
+    nasty_stuff=st.builds(list),
+)
+def test_fuzz_parse_file_as_dict(
+    fn, section_key, version, follow_links, follow_only_interesting_links, nasty_stuff
+):
+    app.parsers.util.parse_file_as_dict(
+        fn=fn,
+        section_key=section_key,
+        version=version,
+        follow_links=follow_links,
+        follow_only_interesting_links=follow_only_interesting_links,
+        nasty_stuff=nasty_stuff,
+    )
+
+
+@given(params=make_linkstr_content())
+def test_fuzz_substitute_linked_content(params):
+    line, linked_content = params
+    print(params)
+    app.parsers.util.substitute_linked_content(linked_content=linked_content, line=line)
+
+
+@given(data=sane_text(), cleanup=st.booleans())
+def test_fuzz_unicode_to_ascii(data, cleanup):
+    app.parsers.util.unicode_to_ascii(data=data, cleanup=cleanup)
