6006a319df304c303cca7a2da0be281ffe4ba4b4
John Maximilian <2e0byo@gmail.com>
2021-03-05 19:18:53 +0000

    actually test everythign

--- backend/app/app/tests/parsers/test_parsers.py
+++ backend/app/app/tests/parsers/test_parsers.py
@@ -8,22 +8,19 @@ from fastapi.testclient import TestClient
 from app import run_parsers
 
 things = ["martyrologies", "psalms", "temporal", "sanctoral", "hymns"]
-things = ["hymns"]
 versions = ["1960"]
 
 
-# @pytest.mark.parametrize("version", versions)
-# def test_pokemon(
-#     version: str,
-#     client: TestClient,
-#     superuser_token_headers: Dict[str, str],
-# ):
-#     """Test all the parsers."""
-#     root = Path("app/tests/parsers/test-DO-data")
-#     lang = "Latin"
+@pytest.mark.parametrize("version", versions)
+def test_pokemon(
+    version: str, client: TestClient, superuser_token_headers: Dict[str, str],
+):
+    """Test all the parsers."""
+    root = Path("app/tests/parsers/test-DO-data")
+    lang = "Latin"
 
-#     # test without upload
-#     run_parsers.parse_upload(root, lang, version, pokemon=True, test=True)
+    # test without upload
+    run_parsers.parse_upload(root, lang, version, pokemon=True, test=True)
 
 
 @pytest.mark.parametrize("version,thing", product(versions, things))
