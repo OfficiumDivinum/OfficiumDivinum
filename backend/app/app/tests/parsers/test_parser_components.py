644d13a4b455f1f904af95a4af9888bdaba06e03
John Maximilian <2e0byo@gmail.com>
2021-03-09 11:54:47 +0000

    antiphon parser test

--- backend/app/app/tests/parsers/test_parser_components.py
+++ backend/app/app/tests/parsers/test_parser_components.py
@@ -1,6 +1,6 @@
 from app import parsers
 from app.parsers import Line
-from app.schemas import LineBase
+from app.schemas import AntiphonCreate, LineBase
 
 
 def test_parse_versicle():
@@ -9,3 +9,10 @@ def test_parse_versicle():
     line = Line(16, " ".join([prefix, content]))
     resp = parsers.parse_versicle(line)
     assert resp == LineBase(lineno=16, content=content, prefix=prefix)
+
+
+def test_parse_antiphon():
+    lineno = 16
+    content = "Antiphon * test content."
+    resp = parsers.parse_antiphon(Line(lineno, content))
+    assert resp == AntiphonCreate(lineno=lineno, content=content)
