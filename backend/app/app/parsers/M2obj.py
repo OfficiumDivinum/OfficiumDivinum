db68bdbc668a6f8a74a98845002507daf4df5a68
John Maximilian <2e0byo@gmail.com>
2021-03-13 22:04:35 +0000

    remove old fn

--- backend/app/app/parsers/M2obj.py
+++ backend/app/app/parsers/M2obj.py
@@ -4,12 +4,10 @@ import re
 from pathlib import Path
 from typing import Optional
 
-from devtools import debug
-
 from app.DSL import days, months, ordinals, specials
 from app.schemas import LineBase, MartyrologyCreate
 
-from .util import parse_DO_sections
+from .chickenfeed import parse_generic_file
 
 christ_the_king_datestr = "Sat between 23 Oct 31 Oct"
 
@@ -28,7 +26,7 @@ def parse_file(fn: Path, lang: str, title: str):
     Martyrology object with rule for specific day.
     """
     if fn.stem == "Mobile":
-        return parse_mobile_file(fn)
+        return parse_generic_file(fn)
 
     month, day = (int(i) for i in fn.stem.split("-"))
     datestr = f"{day} {months[month - 1]}"
@@ -67,45 +65,3 @@ def generate_datestr(section_name: str) -> Optional[str]:
             return christ_the_king_datestr
         elif "Defuncti" in section_name:  # Not sure what we need this for.
             return None
-
-
-def parse_mobile_file(fn: Path, lang: str, title: str):
-    """
-    Parse Martyrology 'Mobile' file.
-
-    Parameters
-    ----------
-    fn: Path : mobile file to parse.
-
-
-    Returns
-    -------
-    List of Martyrology objects with rules, which can be applied.
-    """
-    sections = parse_DO_sections(fn)
-
-    mobile = []
-
-    for datestr, section in sections.items():
-        try:
-            match = re.search(r"(.*?)([0-9]+)-([0-9])", datestr)
-            special = match.group(1)
-            week, day = (int(i) for i in match.group(2, 3))
-            datestr = f"{ordinals[week]} {days[day]} after {specials[special]}"
-        except (AttributeError, IndexError):
-            if datestr == "Nativity":  # hard coded elsewhere.
-                continue
-            elif datestr == "10-DU":
-                datestr = christ_the_king_datestr
-            elif datestr == "Defuncti":  # Not sure what we need this for.
-                continue
-                # datestr = "2 Nov"
-        parts = []
-        for verse in section:
-            parts += [LineBase(**vars(x)) for x in verse]
-
-        mobile.append(
-            MartyrologyCreate(datestr=datestr, parts=parts, title=title, language=lang,)
-        )
-
-    return mobile
