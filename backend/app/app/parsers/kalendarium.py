068a13b986344d8c7733a41e7eac171367f8f801
John Maximilian <2e0byo@gmail.com>
2021-03-14 21:20:59 +0000

    finish port

--- backend/app/app/parsers/kalendarium.py
+++ backend/app/app/parsers/kalendarium.py
@@ -4,18 +4,26 @@ from typing import Dict, Optional, Union
 
 from devtools import debug
 
+from app.DSL import months
+from app.parsers import Line
+from app.parsers.divinumofficium_structures import rank_table_by_calendar
+from app.schemas import CommemorationCreate, FeastCreate
+
+rank_table = None
 version = None
 
 version_names = {"M": "monastic", "NC": "newcal"}
 
 
 def get_version(fn: Path):
-    global version
+    global version, rank_table
     v = fn.stem[1:]
     try:
         version = version_names[v]
     except KeyError:
         version = v
+
+    rank_table = rank_table_by_calendar[version]
 
 
 def num(s: str) -> Union[int, str]:
@@ -39,3 +47,74 @@ def extract_line(line: str) -> Optional[Dict]:
     for match in re.findall(r"((.*?)=([0-9]+\.?[0-9]*)=*)", line):
         data["feasts"].append({"name": match[1], "rank": num(match[2])})
     return data
+
+
+def parse_line(line: Line, language: str, fn: Path) -> Optional[FeastCreate]:
+    """Parse a line of a divinumofficium calendar file."""
+
+    data = extract_line(line.content)
+    month, day = (int(x) for x in data["date"].split("-"))
+    if not data:
+        return None
+    lineno = line.lineno
+    feasts = sorted(data["feasts"], reverse=True, key=lambda x: x["rank"])
+
+    for feast in feasts:
+        if day == 0:
+            feast.update(
+                {"type_": "de Tempore", "datestr": "Sun between 2 Jan 5 Jan OR 2 Jan"}
+            )
+        else:
+            feast.update(
+                {
+                    "type_": "Sanctorum",
+                    "datestr": f"{day} {months[month - 1]}",
+                }
+            )
+        feast.update(
+            {
+                "language": language,
+                "version": version,
+                "sourcefile": fn.name,
+                "lineno": lineno,
+            }
+        )
+        try:
+            feast["rank_name"] = rank_table[feast["rank"]]
+        except ValueError:
+            feast["rank_name"] = rank_table[feast["rank"] + 0.5]
+
+        feast["rank_defeatable"] = True if isinstance(feast["rank"], float) else False
+
+    commemorations = [CommemorationCreate(**x) for x in feasts[1:]]
+    if not commemorations:
+        commemorations = None
+
+    feasts[0]["commemorations"] = commemorations
+    return FeastCreate(**feasts[0])
+
+
+def parse_file(fn: Path, language: str, version: str):
+    """
+    Parse a divinumofficium calendar file.
+
+    Parameters
+    ----------
+    fn: Path : The file to parse.
+
+    version: str : The version to use to generate feast names, etc.
+
+
+    Returns
+    -------
+    A list of FeastCreate objects.
+    """
+    get_version(fn)
+    year = []
+    lines = (Line(i, x) for i, x in enumerate(fn.readlines()))
+    for line in lines:
+        parsed = parse_line(line, language, fn)
+        if parsed:
+            year.append(parsed)
+
+    return year
