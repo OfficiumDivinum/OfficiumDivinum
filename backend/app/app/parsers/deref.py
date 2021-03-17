b593b0762633e893a16bff86638d5bc37b4bdd88
John Maximilian <2e0byo@gmail.com>
2021-03-17 13:49:29 +0000

    was invalid?!

--- backend/app/app/parsers/deref.py
+++ backend/app/app/parsers/deref.py
@@ -10,9 +10,9 @@ def deref(linkstr: str, originf: Path):
     try:
         linkstr, part = re.search(r"@(.*?):(.*):*", linkstr).groups()
         if "s/" in part:
-            part = re.search(r"(.*):s/.*", part).groups()[0]
+            part = re.search(r"(.*):s/.*", part).group(1)
         if (match := re.search(r"(.*):[0-9]+-*[0-9]*", part)) is not None:
-            part = match[1]
+            part = match.group(1)
     except AttributeError:
         linkstr = linkstr.replace("@", "").strip()
         part = None
