e79fa909cc8ac9047084f7d0022f3b355892787a
John Maximilian <2e0byo@gmail.com>
2021-03-19 13:47:14 +0000

    relationships in generic models

--- backend/app/app/models/office_parts.py
+++ backend/app/app/models/office_parts.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
@@ -23,11 +23,26 @@ class FromDOMixin:
     sourcefile = Column(String, index=True)
 
 
+block_line_association_table = Table(
+    "block_line_association_table",
+    Base.metadata,
+    Column("block_id", Integer, ForeignKey("block.id"), primary_key=True),
+    Column("line_id", ForeignKey("line.id"), primary_key=True),
+)
+
+
 class Block(Base, FromDOMixin, BlockMixin):
     """Collections of objects or anything not worth representing individually."""
 
     owner_id = Column(Integer, ForeignKey("user.id"))
     owner = relationship("User", back_populates="blocks")
+    parts = relationship(
+        "Line",
+        secondary=block_line_association_table,
+        back_populates="blocks",
+        lazy="joined",
+        passive_deletes="all",
+    )
 
 
 class Line(Base, FromDOMixin, LineMixin):
@@ -35,3 +50,10 @@ class Line(Base, FromDOMixin, LineMixin):
 
     owner_id = Column(Integer, ForeignKey("user.id"))
     owner = relationship("User", back_populates="lines")
+    blocks = relationship(
+        "Block",
+        secondary=block_line_association_table,
+        back_populates="blocks",
+        lazy="joined",
+        passive_deletes="all",
+    )
